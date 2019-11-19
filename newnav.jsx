4f2a3cc8b51cd25dbb775d50e2132139ef2919b9
Shivesh Gupta <42131203+shivesh947@users.noreply.github.com>
2019-11-19 10:05:25 +0530

    Create newnav.jsx

---
+++ newnav.jsx
@@ -0,0 +1,13 @@
+<nav id="nav" className="navbar navbar-expand-lg navbar-light">
+ <a className="navbar-brand" href="#"><h1> Tourism</h1></a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className="navbar-nav">
+      <a className="nav-item nav-link active" href="#">Home <span className="sr-only">(current)</span></a>
+      <a className="nav-item nav-link" href="#">link2</a>
+      <a className="nav-item nav-link" href="#">link2</a>
+    </div>
+  </div>
+</nav>
